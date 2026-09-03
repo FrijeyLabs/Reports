b519b0f6aed5e669d413effc9256b5d03f6f7b61
Jeffry Marte Bonifacio <80547562+FrijeyLabs@users.noreply.github.com>
2026-09-03 14:24:02 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const CACHE_NAME = "tph-control-v3-logo";
+const CACHE_PREFIX = "tph-control-";
+const APP_SHELL = [
+  "./Tiempo-para-Hombres-Control-de-Produccion.html",
+  "./manifest.webmanifest",
+  "./icon-32.png",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./icon-maskable-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys
+          .filter(key => key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match("./Tiempo-para-Hombres-Control-de-Produccion.html"))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }))
+  );
+});
